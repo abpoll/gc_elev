547ebcbe2f92f33cb757f3b62e10d892283a81e7
Adam B. Pollack <f006dwr@kasper.thayer.dartmouth.edu>
2023-01-28 14:44:29 -0500

    initial project directory from cookiecutter data science

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Building Exposure and Vulnerability Functionality for Risk Estimation from Precalibrated Hazard Model Output',
+    author='Adam Pollack',
+    license='MIT',
+)
