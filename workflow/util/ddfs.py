afbd1523a6d6075841ba3a162fc03a62c82d660e
Adam Pollack <adampollack25@gmail.com>
2023-11-06 17:30:12 -0500

    generated risk ensemble

--- workflow/util/ddfs.py
+++ workflow/util/ddfs.py
@@ -1,5 +1,6 @@
 # Packages
-import pandas
+import pandas as pd
+import numpy as np
 
 '''
 Helpful functions for processing depth-damage functions
@@ -51,3 +52,111 @@ def ddf_max_depth_dict(tidy_ddf, dam_col):
     DDF_DICT = dict(zip(max_d_params['ddf_id'], max_d_params[dam_col]))
 
     return DDF_DICT
+
+def est_naccs_loss(bld_types, depths, ddfs, MAX_DICT):
+    # We are going to use a random number generator
+    rng = np.random.default_rng()
+
+    # Combine building types and depths on index
+    bld_depths = pd.concat([bld_types, pd.Series(depths)], axis=1)
+    # Rename columns to correspond to each series
+    bld_depths.columns = ['ddf_id', 'depth_ft']
+    # Merge bld_type/depths with the ddfs to get params linked up
+    loss_prep = bld_depths.merge(ddfs, how='left')
+    # Helpful to have a mask for where there are no flood depths
+    loss_mask = loss_prep['depth_ft'].notnull()
+    # When depths are null, no flooding so no damages
+    loss_prep.loc[~loss_mask, 'rel_dam'] = 0
+    # There are some depths greater than the support from DDFs
+    # We are going to use the max_d_dict from preparing the DDFs to
+    # assign the params from the max depth for the same bld_type
+    missing_rows = ((loss_mask) &
+                    (loss_prep['params'].isnull()))
+    missing_params = loss_prep.loc[missing_rows]['ddf_id'].map(MAX_DICT)
+
+    # Replace the entries with missing params but positive depths
+    loss_prep.loc[missing_rows, 'params'] = missing_params
+    # Now we can estimate losses for all notnull() depth_ft rows
+    # using the triangular distribution with the 'params' column
+    # Using the loss_mask, which gives us rows with flooding,
+    # we will first get the ddf_params from 'parms' and
+    # use np.stack to access the columns for rng()
+    ddf_params = pd.DataFrame(np.stack(loss_prep.loc[loss_mask]['params']),
+                              columns=['left', 'mode', 'right'],
+                              index=loss_prep.loc[loss_mask].index)
+    # We separate left, mode, max as columns
+    # which makes it much easier to get the damage values we need
+    loss_df = pd.concat([loss_prep.loc[loss_mask], ddf_params], axis=1)
+    
+    # When there is no actual triangular distribution, because values are
+    # equal, just return one of the param values, 
+    no_tri_mask = loss_df['left'] == loss_df['right']
+    loss_df.loc[no_tri_mask, 'rel_dam'] = loss_df.loc[no_tri_mask]['left']
+    
+    # Otherwise we draw
+    # from the triangular distribution
+    loss_df.loc[~no_tri_mask,
+                'rel_dam'] = rng.triangular(loss_df.loc[~no_tri_mask]['left'],
+                                            loss_df.loc[~no_tri_mask]['mode'],
+                                            loss_df.loc[~no_tri_mask]['right'])
+    
+    # Combine to get our losses series
+    fld_loss = loss_df['rel_dam']
+    no_fld = loss_prep[loss_prep['rel_dam'].notnull()]['rel_dam']
+    losses = pd.concat([fld_loss, no_fld], axis=0).sort_index()
+
+    # Return our loss estimates
+    return losses
+
+def est_hazus_loss(hazus_ddf_types, depths, ddfs, MAX_DICT):
+    # We are going to use a random number generator
+    rng = np.random.default_rng()
+
+    # Combine building types and depths on index
+    bld_depths = pd.concat([hazus_ddf_types, pd.Series(depths)], axis=1)
+    # Rename columns to correspond to each series
+    bld_depths.columns = ['ddf_id', 'depth_ft']
+    # Merge bld_type/depths with the ddfs to get params linked up
+    loss_prep = bld_depths.merge(ddfs, how='left')
+    # Helpful to have a mask for where there are no flood depths
+    loss_mask = loss_prep['depth_ft'].notnull()
+    # When depths are null, no flooding so no damages
+    loss_prep.loc[~loss_mask, 'rel_dam'] = 0
+    # There are some depths greater than the support from DDFs
+    # We are going to use the max_d_dict from preparing the DDFs to
+    # assign the params from the max depth for the same bld_type
+    missing_rows = ((loss_mask) &
+                    (loss_prep['params'].isnull()))
+    missing_params = loss_prep.loc[missing_rows]['ddf_id'].map(MAX_DICT)
+    # Replace the entries with missing params but positive depths
+    loss_prep.loc[missing_rows, 'params'] = missing_params
+    # Now we can estimate losses for all notnull() depth_ft rows
+    # using the uniform distribution with the 'params' column
+    # Using the loss_mask, which gives us rows with flooding,
+    # we will first get the ddf_params from 'parms' and
+    # use np.stack to access the columns for rng()
+    ddf_params = pd.DataFrame(np.stack(loss_prep.loc[loss_mask]['params']),
+                              columns=['low', 'high'],
+                              index=loss_prep.loc[loss_mask].index)
+    # We separate low and high as columns
+    # which makes it much easier to get the damage values we need
+    loss_df = pd.concat([loss_prep.loc[loss_mask], ddf_params], axis=1)
+    
+    # When values are
+    # equal, just return one of the param values, 
+    no_uni_mask = loss_df['low'] == loss_df['high']
+    loss_df.loc[no_uni_mask, 'rel_dam'] = loss_df.loc[no_uni_mask]['low']
+    
+    # Otherwise we draw
+    # from the triangular distribution
+    loss_df.loc[~no_uni_mask,
+                'rel_dam'] = rng.uniform(loss_df.loc[~no_uni_mask]['low'],
+                                         loss_df.loc[~no_uni_mask]['high'])
+    
+    # Combine to get our losses series
+    fld_loss = loss_df['rel_dam']
+    no_fld = loss_prep[loss_prep['rel_dam'].notnull()]['rel_dam']
+    losses = pd.concat([fld_loss, no_fld], axis=0).sort_index()
+
+    # Return our loss estimates
+    return losses
